069f5673e10f50d702b68f38ddbc9cae18cc33d6
Andrés Fuentealba <p.andresfuentealbabello@gmail.com>
2026-04-09 04:58:58 +0000

    EN EL CUAL ESTÁN ESO SACALO, DE LOS DERECHOS RESERVADOS

--- src/components/install-pwa-button.tsx
+++ src/components/install-pwa-button.tsx
@@ -131,7 +131,7 @@ export function InstallPWAButton() {
               <span className="text-[9px] font-black uppercase tracking-tighter">Software Protegido</span>
             </div>
             <p className="text-[8px] font-bold text-center leading-tight uppercase tracking-[0.1em] text-slate-500 max-w-[200px]">
-              PATENTADO POR PABLO FB, EN EL CUAL ESTÁN TODOS LOS DERECHOS RESERVADOS
+              PATENTADO POR PABLO FB, TODOS LOS DERECHOS RESERVADOS
             </p>
           </div>
         </div>
